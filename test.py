b4261e56f2e5a8defa05a3e2fc7b63d4769c7d19
ksanu <j.plebaniak@gmail.com>
2019-05-05 20:56:21 +0200

    Mushroom LogisticR, finished

--- test.py
+++ test.py
@@ -23,9 +23,9 @@ y_temp= yX_train['y'].values
 y_train = np.where(y_temp == 'p', 0, 1)
 #print (y_train)
 X_train = yX_train.drop('y', axis=1)
-#print (X_train)
+print (X_train)
 X_train_one_hot = pd.get_dummies( X_train)
-#print (X_train_one_hot)
+print (X_train_one_hot)
 
 #Zbiór testowy:
 columnNames2 = ['cap-shape',
@@ -39,7 +39,8 @@ columnNames2 = ['cap-shape',
                       'gill-color', 'stalk-shape', 'stalk-root', 'stalk-surface-above-ring', 'stalk-surface-below-ring',
 'stalk-color-above-ring','stalk-color-below-ring', 'veil-type','veil-color', 'ring-number', 'ring-type',
                       'spore-print-color', 'population', 'habitat']
-X_test = pd.DataFrame(pd.read_csv('test-A/in.tsv', encoding="utf-8", delimiter='\t', header=None, names=columnNames2))
+X_test = pd.DataFrame(pd.read_csv('dev-0/in.tsv', encoding="utf-8", delimiter='\t', header=None, names=columnNames2))
+y_test = pd.DataFrame(pd.read_csv('dev-0/expected.tsv', encoding="utf-8", delimiter='\t', header=None))
 
 X_test_one_hot = pd.get_dummies(X_test)
 
@@ -71,12 +72,20 @@ reg = LogisticRegression()
 reg.fit(X_train_one_hot, y_train)
 
 y_out_predicted = reg.predict(fixed_X_test_one_hot)
-#print (y_out_predicted)
+print (y_out_predicted)
 y_out = np.where(y_out_predicted == 1, 'e', 'p')
 
+score = accuracy_score(y_test, y_out)
+print(score)
+"""
+X_test_in = pandas.read_csv('test-A/in.tsv', sep='\t', header=None, usecols=[0, 1, 3, 8]).fillna(-1)
+
+reg = LogisticRegression().fit(X_train, y_train)
+
+y_out = reg.predict(X_test_in)
 
 with open('test-A/out.tsv', 'w') as output_file:
     for out in y_out:
-        print('%s' % out, file=output_file)
+        print('%.0f' % out, file=output_file)
 
-
+"""
